07a1e376fbb2ade7fe318f5dafa2e802e50c1657
Joseph Tsindos <jtsindos@gmail.com>
2018-09-19 18:56:59 +0800

    Fix bugs with import script.

--- import/confirmIds.js
+++ import/confirmIds.js
@@ -23,30 +23,47 @@ const confirmIds = async (DIR) => {
      * This means, for a given JSON export file, the fieldGroup in that file does not have a customPostType in the location,
      * and so is assumed to be tied to a specific page or post ID.
      */
-    const [ json, fieldGroups ] = store.parseFile(file, DIR)
+    const fieldGroups = JSON.parse(fs.readFileSync(path.join(__dirname, '..', DIR, file), 'utf8'))
     const ids = await getIds(fieldGroups)
-    const updatedJson = json.map((j, index) => setId(j, ids[index]))
+    const updatedJson = fieldGroups.map((j, index) => setId(j, ids[index]))
     return new Promise((resolve, reject) => {
       fs.writeFile(path.join(__dirname, '..', DIR, file), JSON.stringify(updatedJson, null, 4), function (err) {
         if (err) {
           console.log(err)
           process.exit()
         }
-
-        console.log(`\n"${file}" was updated with the specified ID.\n`)
+        JSON.stringify(updatedJson) !== JSON.stringify(fieldGroups)
+          ? console.log(`\n"${file}" was updated with the specified ID.\n`)
+          : console.log(`"${file}" was not updated.\n`)
         resolve()
       })
     })
   }, Promise.resolve())
 }
 
+/**
+ * Gets user input for IDs of pages or posts
+ * @param {*} fieldGroups A direct reading of an export file
+ * [
+ *   {
+ *     title: "Orange Information",
+ *     location:
+ *     [ // locations of first fieldGroup
+ *       [
+ *         // location 1
+ *         // location 2
+ *       ]
+ *     ]
+ *   }
+ * ]
+ */
 const getIds = async (fieldGroups) => {
   const args = await fieldGroups.reduce(async (promise, fieldGroup) => {
     let argument
     const args = await promise
-    if (!fieldGroup.resolverFieldGroup) {
+    if (Number.isInteger(Number(fieldGroup.location[0][0].value))) {
       argument = await captureArgumentWithMessage(
-        `\x1b[1m\x1b[41m\x1b[37mPlease enter the ID of the page or post associated with the field group "${fieldGroup.fullName}" \
+        `\x1b[1m\x1b[41m\x1b[37mPlease enter the ID of the page or post associated with the field group "${fieldGroup.title}" \
 (Press "enter" to leave unchanged).\x1b[0m\n`
       )
     }

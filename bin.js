005126aad2c1615b1f01718960c0107b45b8509b
Joseph Tsindos <jtsindos@gmail.com>
2018-09-20 19:10:18 +0800

    Add server command to executable.

--- bin.js
+++ bin.js
@@ -2,25 +2,31 @@
 
 const importAcf = require('./import')
 const buildSchema = require('./generate')
+const server = require('./server')
 
 let isImportAcf = false
 let isBuildSchema = false
+let isServer = false
 
 process.argv.slice(2).filter(function (arg) {
   if (arg.includes('import')) {
     isImportAcf = true
   } else if (arg.includes('build-schema')) {
     isBuildSchema = true
+  } else if (arg.includes('server')) {
+    isServer = true
   }
 })
 
-function go () {
+async function go () {
   if (isImportAcf) {
     importAcf()
   } else if (isBuildSchema) {
     buildSchema()
+  } else if (isServer) {
+    server()
   } else {
-    console.log('Usage: acfql <import|build-schema>')
+    console.log('Usage: acfql <import|build-schema|server>')
     console.log('')
     console.log('  Runs an acfql command.')
     console.log('')
@@ -28,6 +34,7 @@ function go () {
     console.log('')
     console.log('  import       Run acf import')
     console.log('  build-schema Build acfql schema')
+    console.log('  server Runs the graphql server')
     process.exit()
   }
 }
--- server.js
+++ server.js
@@ -4,28 +4,32 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const fs = require('fs')
 
-if (!fs.existsSync('./settings.js')) {
-  console.log('Please create settings.js by copying settings.example.js. Fill out the relevant database details.')
-  process.exit()
+const main = () => {
+  if (!fs.existsSync('./settings.js')) {
+    console.log('Please create settings.js by copying settings.example.js. Fill out the relevant database details.')
+    process.exit()
+  }
+
+  const { executableSchema } = require('./schema')
+
+  const SERVER_PORT = 3001
+
+  const server = express()
+  server.use(cors())
+
+  server.use('/graphql', bodyParser.json(), graphqlExpress((req) => {
+    return {
+      schema: executableSchema
+    }
+  }))
+
+  server.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+  }))
+
+  server.listen(SERVER_PORT, () => {
+    console.log(`App is now running on http://localhost:${SERVER_PORT}`)
+  })
 }
 
-const { executableSchema } = require('./schema')
-
-const SERVER_PORT = 3001
-
-const server = express()
-server.use(cors())
-
-server.use('/graphql', bodyParser.json(), graphqlExpress((req) => {
-  return {
-    schema: executableSchema
-  }
-}))
-
-server.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql'
-}))
-
-server.listen(SERVER_PORT, () => {
-  console.log(`App is now running on http://localhost:${SERVER_PORT}`)
-})
+module.exports = main

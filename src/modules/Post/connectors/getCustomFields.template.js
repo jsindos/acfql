efe6cd35fcf100fde12753429cefe0f9a11eb9ad
Joseph Tsindos <jtsindos@gmail.com>
2018-09-25 18:31:18 +0800

    Successfully pass previously failing test.

--- src/modules/Post/connectors/getCustomFields.template.js
+++ src/modules/Post/connectors/getCustomFields.template.js
@@ -7,15 +7,19 @@ const updateObjectInObject = (object, key, update) => {
   return Object.entries(object).map(([ k, item ]) => {
     if (k !== key) {
       // This isn't the item we care about - keep it as-is
-      return item
+      return {
+        [k]: item
+      }
     }
 
     // Otherwise, this is the one we want - return an updated value
     return {
-      ...item,
-      ...update
+      [k]: {
+        ...item,
+        ...update
+      }
     }
-  })
+  }).reduce((a, c) => ({ ...a, ...c }), {})
 }
 
 module.exports = function (Post, Postmeta) {
@@ -28,7 +32,7 @@ module.exports = function (Post, Postmeta) {
    * the associated name from fieldNames
    */
   const Connectors = customFieldConnectors({ Post, Postmeta })  
-  const groupedFieldNames = ${JSON.stringify(fieldGroups.map(fieldGroup => ({ [fieldGroup.fullCaseName]: fieldGroup.fields.map(f => f.fullName) })).reduce((a, c) => ({ ...a, ...c }), {}))}
+  const groupedFieldNames = ${JSON.stringify(fieldGroups.map(fieldGroup => ({ [fieldGroup.fullCaseName.charAt(0).toLowerCase() + fieldGroup.fullCaseName.substr(1)]: fieldGroup.fields.map(f => f.fullName) })).reduce((a, c) => ({ ...a, ...c }), {}))}
   const fieldNames = [ '${fieldGroups.reduce((prev, current) => [ ...prev, ...current.fields ], []).map(f => f.fullName).join('\', \'')}' ]
   const renamedConnectors = Object.values(Connectors)
     .map((a, i) => ({ [fieldNames[i]]: a })).reduce((a, c) => ({ ...a, ...c }), {})
@@ -40,7 +44,7 @@ module.exports = function (Post, Postmeta) {
     ).then(values => values.reduce((a, c) => {
       [ currentField, _ ] = Object.entries(c)[0]
       return updateObjectInObject(a, Object.entries(groupedFieldNames).find(([ fieldGroup, fields ]) => fields.find(field => field === currentField))[0], c)
-    }, ${JSON.stringify(fieldGroups.map(fieldGroup => ({ [fieldGroup.fullCaseName]: {} })).reduce((a, c) => ({ ...a, ...c }), {}))}))
+    }, ${JSON.stringify(fieldGroups.map(fieldGroup => ({ [fieldGroup.fullCaseName.charAt(0).toLowerCase() + fieldGroup.fullCaseName.substr(1)]: {} })).reduce((a, c) => ({ ...a, ...c }), {}))}))
   }
 }
 `

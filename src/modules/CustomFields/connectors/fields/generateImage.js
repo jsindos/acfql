8c53f32771c214f7b0ed80fbb685d8f72f24c830
Joseph Tsindos <jtsindos@gmail.com>
2018-10-14 16:30:46 +0530

    Update acfql image field to not locate based on field ID.

--- src/modules/CustomFields/connectors/fields/generateImage.js
+++ src/modules/CustomFields/connectors/fields/generateImage.js
@@ -5,29 +5,19 @@ const getField = require('./utility/getField')
 
 module.exports = (Post, Postmeta) => {
   return function ({ postId }) {
-    return Post.findOne(getField('${field.fieldName}')).then(post => {
-      if (!post) return null
-      return Postmeta.findOne({
+    return Postmeta.findOne({
+      where: {
+        meta_key: '${field.fieldName}',
+        post_id: postId
+      }
+    }).then(postMeta => {
+      if (!postMeta) return null
+      return Post.findOne({
         where: {
-          meta_value: post.post_name
+          id: Number(postMeta.meta_value)
         }
-      }).then(postMeta => {
-        if (!postMeta) return null
-        return Postmeta.findOne({
-          where: {
-            meta_key: postMeta.meta_key.substring(1),
-            post_id: postId
-          }
-        })
-      }).then(postMeta => {
-        if (!postMeta) return null
-        return Post.findOne({
-          where: {
-            id: Number(postMeta.meta_value)
-          }
-        }).then(post => {
-          return post && post.guid
-        })
+      }).then(post => {
+        return post && post.guid
       })
     })
   }
